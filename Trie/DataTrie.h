fffda7465a9f5985fe4b7b1b70b9fd636e76362c
lepoidev <lepoidev@ualberta.ca>
2020-05-22 14:26:01 -0700

    Fixed DataTrieNode inheritance and added additional insert fns

--- Trie/DataTrie.h
+++ Trie/DataTrie.h
@@ -3,7 +3,18 @@
 #include "BasicTrie.h"
 
 template< typename charTy, typename dataTy >
-class DataTrie : BasicTrie< DataTrieNode< dataTy, charTy >, charTy >
+class DataTrie : public BasicTrie< DataTrieNode< dataTy, charTy >, charTy >
 {
+public:
+  std::shared_ptr< DataTrieNode< dataTy, charTy > > const Insert( std::basic_string< charTy > str, dataTy data )
+  {
+    return Insert( str.begin(), str.end(), data );
+  }
 
+  template< typename IterTy >
+  std::shared_ptr< DataTrieNode< dataTy, charTy > > const Insert( IterTy begin, IterTy end, dataTy data )
+  {
+    auto& inserted { DataTrieNode< dataTy, charTy >::Insert( this->m_root, begin, end ) };
+    inserted.SetVal( data );
+  }
 };
--- Trie/DataTrieNode.h
+++ Trie/DataTrieNode.h
@@ -1,28 +1,46 @@
 #pragma once
 #include "TrieNode.h"
 
-template< typename valTy, typename charTy >
+template< typename dataTy, typename charTy >
 class DataTrieNode : TrieNode< charTy >
 {
 public:
   #pragma region Constructors
-  DataTrieNode( valTy val, charTy charVal )
-    : DataTrieNode( charVal ), m_val { val }
-  {}
+  DataTrieNode( dataTy data, charTy charVal )
+    : DataTrieNode( charVal ), m_data { data }
+  {
+
+  }
 
   DataTrieNode( charTy charVal )
-    : TrieNode<charTy>( charVal ), m_val {}
-  {}
+    : TrieNode<charTy>( charVal ), m_data {}
+  {
+    static_assert( !std::is_same<charTy, dataTy>::value, "Cannot use same type for data as char." );
+  }
 
-  DataTrieNode( valTy val )
-    : DataTrieNode(), m_val { val }
-  {}
+  DataTrieNode( dataTy data )
+    : DataTrieNode(), m_data { data }
+  {
+
+  }
 
   DataTrieNode()
     : DataTrieNode( (charTy)0 )
-  {}
+  {
+
+  }
   #pragma endregion
 
+  #pragma region Getters / Setters
+  void SetVal( dataTy const& data )
+  {
+    m_data = data;
+  }
+  dataTy const GetVal() const
+  {
+    return m_data;
+  }
+  #pragma endregion
 protected:
-  valTy m_val;
+  dataTy m_data;
 };
